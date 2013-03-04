fd02b48529a9f3356e653cac4bfa91db3bb1b86c
Jason Gregori <jasongregori@gmail.com>
2013-03-03 19:21:04 -0800

    Added empty RXInterpolationTests file

---
+++ RXPreprocessingTests/RXInterpolationTests.h
@@ -0,0 +1,13 @@
+//
+//  RXInterpolationTests.h
+//  RXPreprocessingTests
+//
+//  Created by Jason Gregori on 3/3/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RXInterpolationTests : SenTestCase
+
+@end
---
+++ RXPreprocessingTests/RXInterpolationTests.m
@@ -0,0 +1,15 @@
+//
+//  RXInterpolationTests.m
+//  RXPreprocessingTests
+//
+//  Created by Jason Gregori on 3/3/13.
+//
+//
+
+#import "RXInterpolationTests.h"
+
+#import "RXInterpolation.h"
+
+@implementation RXInterpolationTests
+
+@end
